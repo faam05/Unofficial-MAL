64ea4cff61e76c56c9308b04b40e36777907ba2e
faam05 <fadilfadoli30@gmail.com>
2023-03-01 23:51:48 +0700

    dynamic for searching view

--- src/pages/search/index.jsx
+++ src/pages/search/index.jsx
@@ -1,4 +1,5 @@
 import { Anchor, Badge, Card, CardSection, Center, Flex, Grid, Group, Image, Rating, SimpleGrid, Spoiler, Stack, Text } from '@mantine/core'
+import { useMediaQuery } from '@mantine/hooks'
 import { IconBrandYoutube } from '@tabler/icons'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
@@ -15,32 +16,35 @@ export default function Search() {
   useEffect(() => {
     getData()
   }, [params])
+
+  const matches = useMediaQuery('(min-width: 768px)')
+
   return (
     <>
       <Text>Result</Text>
-      <SimpleGrid cols={2} style={{ marginTop: '10px' }}>
-        {data ? (
-          data.length > 0 ? (
-            data.map((item, index) => {
+      {data ? (
+        data.length > 0 ? (
+          <SimpleGrid cols={matches ? 2 : 1} style={{ marginTop: '10px' }}>
+            {data.map((item, index) => {
               return (
                 <Card key={index} shadow='sm' radius='md'>
                   <SimpleGrid cols={2}>
                     <div>
                       <Center>
                         <Flex direction={'column'}>
-                          <Image height={250} fit='contain' mr={10} src={item.images.jpg.image_url} />
+                          <Image height={matches ? 250 : 150} fit='contain' mr={10} src={item.images.jpg.image_url} />
                           <Text ta={'center'} fz={'xs'}>
                             {item.aired.string}
                           </Text>
                           <Flex justify={'space-evenly'}>
-                            <Rating value={item.score / 2} fractions={2} readOnly />
-                            <Badge fz={'xs'}>{Number(item.score).toFixed(2)}</Badge>
+                            <Rating size={matches ? 'md' : 'xs'} value={item.score / 2} fractions={2} readOnly />
+                            <Badge size={matches ? 'md' : 'xs'}>{Number(item.score).toFixed(2)}</Badge>
                           </Flex>
                           <Text ta={'center'}>
                             {item.studios.length > 0
                               ? item.studios.map((studio, index) => {
                                   return (
-                                    <Badge key={index} m='10px 1px' color={'gray'}>
+                                    <Badge key={index} m='10px 1px' color={'gray'} size={matches ? 'md' : 'xs'}>
                                       {studio.name}
                                     </Badge>
                                   )
@@ -57,41 +61,49 @@ export default function Search() {
                       </Center>
                     </div>
                     <div>
-                      <Text mt={10} fz='md'>
+                      <Text mt={matches ? 10 : 0} fz={matches ? 'md' : 'sm'}>
                         <Anchor href={`/detail/${item.mal_id}`}>{item.title}</Anchor>
                       </Text>
-                      <Flex mt={10} fz='sm'>
-                        <Text>
+                      <Flex mt={matches ? 10 : 5}>
+                        <Text fz={matches ? 'md' : 11}>
                           {item.type} {item.episodes ? `(${item.episodes} episodes)` : `( ${item.status} )`}
                         </Text>
                         {item.trailer.url ? (
-                          <a target={'_blank'} style={{ marginLeft: 10 }} href={item.trailer.url}>
+                          <a target={'_blank'} style={{ marginLeft: matches ? 10 : 0 }} href={item.trailer.url}>
                             <IconBrandYoutube />
                           </a>
                         ) : null}
                       </Flex>
-                      {item.genres.map((genre, index) => {
-                        return (
-                          <Badge key={index} m='10px 1px' color={'gray'}>
-                            {genre.name}
-                          </Badge>
-                        )
-                      })}
-                      <Spoiler maxHeight={120} showLabel='Show more' hideLabel='Hide' mt={10} fz='sm'>
-                        {item.synopsis}
-                      </Spoiler>
+                      <SimpleGrid cols={2} mt={10}>
+                        {item.genres.map((genre, index) => {
+                          return (
+                            <Badge key={index} size={matches ? 'md' : 'xs'} color={'gray'}>
+                              {genre.name}
+                            </Badge>
+                          )
+                        })}
+                      </SimpleGrid>
+                      {matches ? (
+                        <Spoiler maxHeight={120} showLabel='Show more' hideLabel='Hide' mt={10} fz='sm'>
+                          {item.synopsis}
+                        </Spoiler>
+                      ) : (
+                        <Text mt={10} fz='xs'>
+                          {item.synopsis.substring(0, 100) + (item.synopsis.length > 100 ? '...' : '')}
+                        </Text>
+                      )}
                     </div>
                   </SimpleGrid>
                 </Card>
               )
-            })
-          ) : (
-            <p>Tidak ada data</p>
-          )
+            })}
+          </SimpleGrid>
         ) : (
-          <p>Loading...</p>
-        )}
-      </SimpleGrid>
+          <p>Tidak ada data</p>
+        )
+      ) : (
+        <p>Loading...</p>
+      )}
     </>
   )
 }
