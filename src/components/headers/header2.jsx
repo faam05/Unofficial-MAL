6d7f9c423fca7ed3be6c0f1465669f897cce7de8
faam05 <fadilfadoli30@gmail.com>
2023-08-03 12:57:01 +0700

    feat: responsive home

--- src/components/headers/header2.jsx
+++ src/components/headers/header2.jsx
@@ -1,59 +1,11 @@
-import { Autocomplete, Burger, Button, Container, Group, Header, Image, MediaQuery, Text, useMantineTheme } from '@mantine/core'
-import { IconSearch } from '@tabler/icons'
-import axios from 'axios'
-import React, { forwardRef, useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import useDebounce from '../../hooks/useDebounce'
+import { Container, Header } from '@mantine/core'
+import React from 'react'
+import DesktopSearch from './Desktop/DesktopSearch'
+import MobileSearch from './Mobile/MobileSearch'
+import { useMediaQuery } from '@mantine/hooks'
 
 export default function CustomHeader2() {
-  const theme = useMantineTheme()
-  const navigate = useNavigate()
-
-  const [searchTerm, setSearchTerm] = useState('')
-  const [results, setResults] = useState([])
-
-  const [opened, setOpened] = useState(false)
-  const [loading, setLoading] = useState(false)
-
-  const AutoCompleteItem = forwardRef(({ description, value, image, ...others }, ref) => (
-    <>
-      <div ref={ref} {...others}>
-        <Group noWrap>
-          <Image width={60} height={70} src={image} />
-          <div>
-            <Text size={'xs'}>{value}</Text>
-            <Text size='xs' c='dimmed'>
-              {description}
-            </Text>
-          </div>
-        </Group>
-      </div>
-    </>
-  ))
-
-  const debouncedSearchTerm = useDebounce(searchTerm, 500)
-
-  useEffect(() => {
-    setLoading(true)
-    if (debouncedSearchTerm) {
-      const fetchResults = async () => {
-        await axios(`https://api.jikan.moe/v4/anime?q=${debouncedSearchTerm}`).then((res) => {
-          setResults(
-            res.data.data.map((item) => ({
-              group: item.genres.length > 0 ? item.genres[0].type : 'Unknown',
-              value: item.title,
-              description: `(${item.type}, ${item.year})`,
-              id: item.mal_id,
-              image: item.images.jpg.small_image_url,
-            }))
-          )
-        })
-        setLoading(false)
-      }
-      fetchResults()
-    }
-    setLoading(false)
-  }, [debouncedSearchTerm])
+  const matches = useMediaQuery('(min-width: 720px)')
 
   return (
     <Header height={{ base: 50, md: 70 }} p='md'>
@@ -65,46 +17,7 @@ export default function CustomHeader2() {
           height: '100%',
           justifyContent: 'space-between',
         }}>
-        <MediaQuery largerThan='sm' styles={{ display: 'none' }}>
-          <Burger opened={opened} onClick={() => setOpened((o) => !o)} size='sm' color={theme.colors.gray[6]} mr='xl' />
-        </MediaQuery>
-        <MediaQuery largerThan='sm' styles={{ display: 'none' }}>
-          <Text component='a' href='/'>
-            MAL
-          </Text>
-        </MediaQuery>
-        <MediaQuery smallerThan='md' styles={{ display: 'none' }}>
-          <Text component='a' href='/'>
-            MAL
-          </Text>
-        </MediaQuery>
-        <MediaQuery smallerThan='md' styles={{ display: 'none' }}>
-          <Group spacing={5}>
-            <Button
-              variant={location.pathname != '/' ? 'subtle' : 'filled'}
-              style={{
-                marginRight: 10,
-              }}
-              onClick={() => navigate('/')}>
-              <Text>Home</Text>
-            </Button>
-            <Autocomplete
-              dropdownComponent={({ children, ...others }) => <div style={{ width: 300, maxHeight: 300, overflow: 'auto' }}>{children}</div>}
-              nothingFound={debouncedSearchTerm ? (loading ? 'Loading...' : 'No results found') : 'Type to search'}
-              value={searchTerm}
-              limit={30}
-              itemComponent={AutoCompleteItem}
-              placeholder={'Search'}
-              onChange={(e) => setSearchTerm(e)}
-              icon={<IconSearch size={16} stroke={1.5} />}
-              data={results}
-              onItemSubmit={(item) => {
-                setValue('')
-                navigate(`/detail/${item.id}`)
-              }}
-            />
-          </Group>
-        </MediaQuery>
+        {matches ? <DesktopSearch /> : <MobileSearch />}
       </Container>
     </Header>
   )
---
+++ src/pages/home/Mobile/HomeMobile.jsx
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from 'react'
+import { Image, Text } from '@mantine/core'
+import { createStyles } from '@mantine/core'
+import axios from 'axios'
+import { NavLink } from 'react-router-dom'
+import { Carousel } from '@mantine/carousel'
+import { useMediaQuery } from '@mantine/hooks'
+
+const useStyles = createStyles((_theme, _params, getRef) => ({
+  controls: {
+    ref: getRef('controls'),
+    transition: 'opacity 150ms ease',
+    opacity: 0,
+  },
+
+  root: {
+    '&:hover': {
+      [`& .${getRef('controls')}`]: {
+        opacity: 1,
+      },
+    },
+  },
+}))
+
+function HomeMobile(props) {
+  const { classes } = useStyles()
+
+  const breakpoints = {
+    xs: 500,
+    sm: 800,
+    md: 1000,
+    lg: 1200,
+    xl: 1400,
+  }
+
+  return (
+    <>
+      <Text style={{ borderColor: '#bebebe', borderStyle: 'solid', borderWidth: '0 0 1px' }} transform='capitalize'>
+        {props.data[0].season} {props.data[0].year} anime
+      </Text>
+      <Carousel
+        controlSize={40}
+        height='auto'
+        slideSize='162'
+        slideGap='sm'
+        loop
+        align='start'
+        breakpoints={[
+          { maxWidth: 'md', slideSize: '50%' },
+          { maxWidth: 'sm', slideSize: '10%', slideGap: 'sm' },
+        ]}
+        style={{
+          position: 'relative',
+        }}
+        classNames={classes}>
+        {props.data.map((item, index) => {
+          return (
+            <Carousel.Slide
+              key={index}
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+                marginTop: '5px',
+              }}>
+              <NavLink
+                to={`/detail/${item.mal_id}`}
+                style={{
+                  position: 'relative',
+                }}>
+                <Image imageProps={{ loading: 'loading' }} height={220} width={160} src={item.images.jpg.image_url} withPlaceholder />
+                <Text
+                  style={{
+                    width: '100%',
+                    fontSize: '11px',
+                    fontWeight: 400,
+                    padding: '15px 5px 5px',
+                    bottom: 0,
+                    position: 'absolute',
+                    color: '#fff',
+                    background: `linear-gradient(0deg, rgba(0,0,0,1) 30%, rgba(255,255,255,0) 100%)`,
+                  }}>
+                  {item.title}
+                </Text>
+              </NavLink>
+            </Carousel.Slide>
+          )
+        })}
+      </Carousel>
+      <Text
+        style={{
+          marginTop: '15px',
+          borderColor: '#bebebe',
+          borderStyle: 'solid',
+          borderWidth: '0 0 1px',
+        }}>
+        Latest Updated Episode Videos
+      </Text>
+    </>
+  )
+}
+
+export default HomeMobile
