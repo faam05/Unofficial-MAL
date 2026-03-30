fdaa2626e6dfe525f74f71d62737453073a5fec9
faam05 <fadilfadoli30@gmail.com>
2026-03-30 16:37:57 +0700

    debug: main app

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,19 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { MantineProvider } from '@mantine/core'
 import { BrowserRouter } from 'react-router-dom'
+
 import App from './App'
+
 import './styles/main.css'
 import '@mantine/core/styles.css'
 import '@mantine/carousel/styles.css'
 
+const isStream = window.location.hostname.startsWith('stream')
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={isStream ? '/stream' : '/'}>
       <MantineProvider>
         <App />
       </MantineProvider>
